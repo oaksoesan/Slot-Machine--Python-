d1074ec3619411cf94f1c079fac9bc3345434513
oaksoesan <oakso3san.96@gmail.com>
2023-06-30 09:44:35 +0630

    printout slot in format

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbol = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbol.append(symbol)            
+        
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbol[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+    
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end= "|")
+            else:
+                print(column[row], end="")
+        print()
+
 
 
 def deposit():
@@ -52,6 +92,18 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bets = get_bet()
-    print(balance, lines, bets)
-main()
+    while True:
+        bets = get_bet()
+        total_bets = bets * lines
+        if total_bets > balance:
+            print(f"You do not have enough balance. Your current balance is ${balance}")
+        else:
+            break
+
+    print(
+        f"You are betting ${bets} on {lines} lines. Total bet is equal to: ${total_bets} ")
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+
+
+main()  
